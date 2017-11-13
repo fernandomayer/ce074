ef3e101df6e4619b805d0c1aa63422bfafb9bffb
Fernando Mayer <fernandomayer@gmail.com>
2017-11-13 17:32:38 -0200

    Arruma script de respostas

--- scripts/fatorial_fracionado-respostas.R
+++ scripts/fatorial_fracionado-respostas.R
@@ -15,14 +15,14 @@ dados
 dados$ABCDE <- with(dados, A*B*C*D*E)
 ## ordena pelos sinais dessa coluna
 dados[order(dados$ABCDE, decreasing = TRUE), ]
-## As combações com sinal positivo de ABCDE podem ser a fração
+## As combições com sinal positivo de ABCDE podem ser a fração
 ## principal, portanto, fazemos a seleção desses elementos
 dados <- subset(dados, ABCDE == 1)
 ## Portanto, as combinaçoes seriam
 row.names(dados)
 
 ## b)
-## As relaçãoes associadas devem ser encontradas manualmente, ou usando
+## As relações associadas devem ser encontradas manualmente, ou usando
 ## a função abaixo
 aliases <- function(ef, cd){
     ali <- function(ef, cd){
@@ -42,7 +42,6 @@ aliases <- function(ef, cd){
                x[order(nchar(x))]
            })
 }
-
 ## Cria a tabela de sinais
 (tab <- model.matrix(~ A*B*C*D*E, data = dados))
 ## Aplicando a função, encontrando os pares associados
@@ -54,11 +53,11 @@ aliases(colnames(tab)[-c(1, 32)], "ABCDE")
 (tab <- unique(tab, MARGIN = 2))
 ## Contrastes
 contr <- t(tab[, -1]) %*% dados$Y
-## Efeitos = contraste/(n2^{k-1})
+## Efeitos = contraste/(r2^{k-1})
 ## NOTE que agora o k é 4, pois o experimento básico é 2^{5-1} = 2^4
 k <- 4
-n <- 1
-(ef <- contr/(n * 2^(k-1)))
+r <- 1
+(ef <- contr/(r * 2^(k-1)))
 
 ## d)
 ## Gráfico de probabilidade normal dos efeitos estimados
@@ -66,14 +65,32 @@ qqaux <- qqnorm(ef, col = 2, pch = 19); qqline(ef)
 text(qqaux$x, qqaux$y, rownames(qqaux$y), cex = 0.8, pos = 3)
 
 ## e)
-## Os efeitos iportantes foram A, B, C, AB, e DE. Note que
-## DE . ADCDE = ABC
-## entao podemos especificar o modelo com
-m0 <- lm(Y ~ A + B + C + A:B + A:B:C, data = dados)
+## Os efeitos iportantes foram A, B, C, AB, e DE. Mas para sermos
+## conservadores podemos ajustar um modelo com todos menos aqueles
+## efeitos que estão exatamente na linha.
+m0 <- lm(Y ~ (A + B + C + D + E)^2 - B:D - B:E, data = dados)
 anova(m0)
+## Mantendo os mais importantes
+m1 <- update(m0, . ~ . - A:C - C:D - C:E)
+anova(m1)
+## Nova avaliação
+m2 <- update(m1, . ~ . - A:D - B:C)
+anova(m2)
+## MAS note que
+## DE = ABC
+## AE = BCD
+## Portanto podemos usar estas interações e eliminar E
+m3 <- update(m2, . ~ A + B + C + D + A:B + A:B:C + B:C:D)
+anova(m3)
+## Agora podemos testar a exclusão de BCD, e por consequencia de D
+m4 <- update(m3, . ~ . - D - B:C:D)
+anova(m4)
+## Fazendo o TRV
+anova(m3, m4)
+## Portanto, pode-se escolher o modelo com menos parâmetros (m4)
 
 ## f)
-res <- residuals(m0)
+res <- residuals(m4)
 qqnorm(res); qqline(res)
 
 ##======================================================================
@@ -118,11 +135,17 @@ aliases <- function(ef, cd){
                x[order(nchar(x))]
            })
 }
-
 ## Cria a tabela de sinais
 (tab <- model.matrix(~ A*B*C*D*E, data = dados))
+colnames(tab)
+## Note que a interação generalizada de ABD com ACE é
+## (ABD)(ACE) = BCDE
+## Portanto, a relação completa de definição é
+## I = ABD = ACE = BCDE
+## Pode conferir com
+tab[, which(apply(tab, 2, function(x) all(x == 1)))]
 ## Aplicando a função, encontrando os pares associados
-aliases(colnames(tab)[-1], c("ABD", "ACE"))
+aliases(colnames(tab)[-1], c("ABD", "ACE", "BCDE"))
 
 ## c)
 ## Como vimos que existem colunas confundidas, vamos estimar os efeitos
@@ -130,11 +153,11 @@ aliases(colnames(tab)[-1], c("ABD", "ACE"))
 (tab <- unique(tab, MARGIN = 2))
 ## Contrastes
 contr <- t(tab[, -1]) %*% dados$y
-## Efeitos = contraste/(n2^{k-1})
+## Efeitos = contraste/(r2^{k-1})
 ## NOTE que agora o k é 3, pois o experimento básico é 2^{5-2} = 2^3
 k <- 3
-n <- 1
-(ef <- contr/(n * 2^(k-1)))
+r <- 1
+(ef <- contr/(r * 2^(k-1)))
 
 ## d)
 ## Gráfico de probabilidade normal dos efeitos estimados
